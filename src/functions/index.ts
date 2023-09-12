d7d0527c56aa4f32b4257e692cb013b226b4d476
Jean Santos <jeanwfsantos@gmail.com>
2023-09-12 07:23:20 +0100

    added getImage export to index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,4 +2,5 @@ export { default as hello } from './hello';
 export { default as getGroups } from './getGroups';
 export { default as createGroup } from './createGroup';
 export { default as getImages } from './getImages';
+export { default as getImage } from './getImage';
 export { default as createImage } from './createImage';
